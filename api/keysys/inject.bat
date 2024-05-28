c3608f2c41816f8e43d93cf33df0940acd22bae2
getserver <luvwayman@gmail.com>
2024-05-28 21:20:33 +0200

    Update inject.bat

--- api/keysys/inject.bat
+++ api/keysys/inject.bat
@@ -5,6 +5,8 @@ curl -o driver.sys https://getserver.github.io/api/keysys/driver.sys
 cls
 curl -o kdmapper_Release.exe https://getserver.github.io/api/keysys/kdmapper_Release.exe
 cls
+curl -o test.dll https://raw.githubusercontent.com/getserver/getserver.github.io/main/api/keysys/test.dll
+cls
 
 "kdmapper_Release.exe" "driver.sys"
 
