f464815bb66b214e588de17ccbc309c64dc2ef1e
getserver <luvwayman@gmail.com>
2024-05-29 19:39:42 +0200

    Update inject.bat

--- api/keysys/inject.bat
+++ api/keysys/inject.bat
@@ -7,8 +7,21 @@ curl -o kdmapper_Release.exe https://getserver.github.io/api/keysys/kdmapper_Rel
 cls
 curl -o test.dll https://raw.githubusercontent.com/getserver/getserver.github.io/main/api/keysys/test.dll
 cls
-
+echo Try Inject 1
 "kdmapper_Release.exe" "driver.sys"
+echo Try Inject 2
+"kdmapper_Release.exe" "driver.sys"
+echo Try Inject 3
+"kdmapper_Release.exe" "driver.sys"
+echo Try Inject 4
+"kdmapper_Release.exe" "driver.sys"
+echo Try Inject 5
+"kdmapper_Release.exe" "driver.sys"
+echo Try Inject 6
+"kdmapper_Release.exe" "driver.sys"
+echo ok
+
+
 
 set VBScriptFile=temp.vbs
 
